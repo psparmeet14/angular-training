ecdccde3de30fa94af93d9fa72d728c2aa78f8d0
Parmeet Singh <psparmeet14@gmail.com>
2022-12-19 23:25:50 +0530

    Components, databinding, directives

---
+++ my-first-angular-project/src/app/servers/servers.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Timestamp } from 'rxjs';
+
+@Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created'
+  serverName: string = 'Testserver';
+  username: string = '';
+  serverCreated: boolean = false;
+  servers: string[] = ['Testserver', 'Testserver 2'];
+  log: Date[] = [];
+  showSecret: boolean = false;
+ 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  onCreateServer(): void {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onToggleDetails(): void {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+}

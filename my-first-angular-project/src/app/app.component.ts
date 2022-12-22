d6fd4e3ab2bf961d86a2eb1db1a9a31df3b8031f
Parmeet Singh <psparmeet14@gmail.com>
2022-12-22 12:02:21 +0530

    feat: communication between components

--- my-first-angular-project/src/app/app.component.ts
+++ my-first-angular-project/src/app/app.component.ts
@@ -5,11 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   styles: [`
-    h3 {
+    h2 {
       color: dodgerblue;
     }
   `]
 })
 export class AppComponent {
   title = 'my-first-angular-project';
+  serverElements = [{type: 'server', name: 'Test server', content: 'Just a test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
--- my-first-angular-project/src/app/app.module.ts
+++ my-first-angular-project/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,

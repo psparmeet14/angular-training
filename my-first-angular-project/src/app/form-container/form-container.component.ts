83216984a08de76a71e9ecdc657f4537c05c7fdd
Parmeet Singh <psparmeet14@gmail.com>
2023-01-04 16:02:09 +0530

    feat: template-driven approach for angular forms

--- my-first-angular-project/src/app/form-container/form-container.component.ts
+++ my-first-angular-project/src/app/form-container/form-container.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,8 +9,52 @@ import { NgForm } from '@angular/forms';
 })
 export class FormContainerComponent {
 
-  onSubmit(form: NgForm) {
-    console.log("submitted!");
-    console.log(form);
+  @ViewChild('f', {static: true}) signupForm: NgForm;
+  defaultQuestion: string = 'teacher';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  isSubmitted: boolean = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log("submitted!");
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.isSubmitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+
 }
